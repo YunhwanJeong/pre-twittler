ba0fabb7382f726299d52c77b164c47c5eac7ba9
Lee Hoyong <igotoweb@gmail.com>
2018-10-23 17:23:55 +0900

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// your code here
+
+// DATA는 이미 작성된 트윗을 표시합니다.
+console.log(DATA)
+
+// generateNewTweet을 호출할 때마다 새로운 트윗을 생성합니다.
+console.log(generateNewTweet());
+
+document.getElementById('test').innerHTML = 'hello twittler, check developer console!';

b8d451558ba4f5231952c3bfa4194a841df89c60
정오 <aidenchung5312@gmail.com:git config --global user.email aidenchung5312@gmail.comclarclearqcvgrtfzadftgg>
2019-08-13 23:55:05 +0900

    SYNC

--- script.js
+++ script.js
@@ -20,20 +20,28 @@ function createTweet(DATA) {
   let tweetsPointer = document.querySelector("#tweets");
 
   for(let tweet of DATA) {
-    let div = document.createElement("div");
+    let tweetDiv = document.createElement("div");
+    tweetDiv.setAttribute("class", "tweet")
 
-    var userText = document.createTextNode(tweet.user);
-    div.appendChild(userText);
-    tweetsPointer.appendChild(userText);
+    let usernameSpan = document.createElement("span");
+    let usernameTextnode = document.createTextNode(tweet.user);
+    usernameSpan.appendChild(usernameTextnode);
+    usernameSpan.setAttribute("class", "username");
+    tweetDiv.appendChild(usernameSpan);
 
-    let br = document.createElement("br");
-    tweetsPointer.appendChild(br);
-    
-    var messageText = document.createTextNode(tweet.message);
-    div.appendChild(messageText);
-    tweetsPointer.appendChild(messageText);
+    let timestampSpan = document.createElement("span");
+    let timestampTextnode = document.createTextNode(tweet.created_at);
+    timestampSpan.appendChild(timestampTextnode);
+    timestampSpan.setAttribute("class", "timestamp");
+    tweetDiv.appendChild(timestampSpan);
 
-    tweetsPointer.appendChild(br);
+    let messageDiv = document.createElement("div");
+    let messageTextnode = document.createTextNode(tweet.message);
+    messageDiv.appendChild(messageTextnode);
+    messageDiv.setAttribute("class", "message");
+    tweetDiv.appendChild(messageDiv);
+
+    tweetsPointer.appendChild(tweetDiv);    
   }
 }
 

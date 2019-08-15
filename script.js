c96a8f53d9fa58d2e1c2f69a6c98f7425e814854
정오 <aidenchung5312@gmail.com:git config --global user.email aidenchung5312@gmail.comclarclearqcvgrtfzadftgg>
2019-08-16 01:01:40 +0900

    SYNC

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ function showTweet(i) {
   let usernameTextnode = document.createTextNode(DATA[i].user);
   usernameSpan.appendChild(usernameTextnode);
   usernameSpan.setAttribute("class", "username");
+  usernameSpan.setAttribute("onclick", "showTimeline(this.textContent)");
   tweetDiv.appendChild(usernameSpan);
 
   let timestampSpan = document.createElement("span");
@@ -37,24 +38,64 @@ function createRandomTweet() {
   let randomTweet = generateNewTweet();
   DATA.push(randomTweet);
   showTweet(DATA.indexOf(randomTweet));
+
+
 }
 
 /**pseudo code
  * 
- * -작성자 이름 클릭하면 타임라인을 볼 수 있도록
+ * 작성자 이름 클릭하면 타임라인을 볼 수 있게 만들기
  * 
- *  -클릭하면 함수 작동
- *   -tweets에서 해당 id 가진 것들만 filter.
+ * -클릭한 user와 일치하는 div만 display 할 수 있어야 함
+ *  -for loop 돌면서 클릭한 username의 textNode와 일치하지 않으면 parent에게 display none
+ *   
  * 
  */
 
-// document.getElementsByClassName("username").onclick = showTimeline;
-// function showTimeline() {
-//   let tweetsPointer = document.getElementById("tweets");
-  
-// }
+function buttonSwitcher() {
+  let btsInBtArea = document.querySelectorAll("#buttonArea button");
 
+  for(let i = 0; i < btsInBtArea.length; i++) {
+    if(btsInBtArea[i].style.display === "none") {
+      btsInBtArea[i].style.display = "inline";
+    } else {
+      btsInBtArea[i].style.display = "none";
+    }
+  }
+}
 
+function showTimeline(thisTextContent) {
+  buttonSwitcher();
+  let usernames = document.querySelectorAll(".username");
+  for(let i = 0; i < usernames.length; i++) {
+    if(thisTextContent !== usernames[i].textContent) {
+      usernames[i].parentElement.style.display = "none";
+    }
+  }
+}
+
+function clearFilter() {
+  buttonSwitcher();
+  let allTweets = document.querySelectorAll(".tweet");
+  for(let i = 0; i < allTweets.length; i++) {
+    if(allTweets[i].style.display === "none") {
+      allTweets[i].removeAttribute("style");
+    }
+  }
+}
+
+function createTypedNewTweet() {
+  let tweet = {};
+  let typedUsername = document.querySelector("#inputUsername input").textContent;
+  let typedMessage = document.querySelector("#inputMessage input").textContent;
+
+  tweet.user = typedUsername;
+  tweet.message = typedMessage;
+  tweet.created_at = new Date().format();
+
+  DATA.push(tweet);
+  showTweet(DATA.indexOf(tweet));
+}
 
 
 
